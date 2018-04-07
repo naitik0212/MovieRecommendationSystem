d2390ce013f6baa98154d0e86bda1704e681cb7b
priyekant <priyekantaghi@gmail.com>
2018-04-07 11:38:53 -0700

    item item

---
+++ combine_tags_and_generes.py
@@ -0,0 +1,16 @@
+import csv
+
+xyz = open('new_tags_generes.csv', "a")
+
+i = 0
+f = open('tags.csv', 'r')
+reader = csv.reader(f)
+for row in reader:
+    if i == 0:
+        continue
+    else:
+        rowToWrite = row[1] +','+row[2]+"\n"
+        xyz.write(rowToWrite)
+    i += 1
+f.close()
+xyz.close()
